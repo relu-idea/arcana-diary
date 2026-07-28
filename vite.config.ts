5b24af46f78d494da83cd59f7f624e42033ba10a
ReLUmind <hjyuu268@gmail.com>
2026-07-29 00:43:58 +0900

    Pages 탭의 Build and deployment - Source 항목이 **GitHub Actions**로 선택되어 있는지 확인

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.GITHUB_ACTIONS ? '/arcana-diary/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
